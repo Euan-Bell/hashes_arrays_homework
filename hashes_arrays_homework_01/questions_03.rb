07d18ddb4f849149f5a1cb6444fbefc26a413abc
Euan <bell_designs@outlook.com>
2018-04-12 10:14:31 +0100

    updated homework exercise C

--- hashes_arrays_homework_01/questions_03.rb
+++ hashes_arrays_homework_01/questions_03.rb
@@ -1,36 +1,36 @@
-united_kingdom = [
-  {
-    name: "Scotland",
-    population: 5295000,
-    capital: "Edinburgh"
-  },
-  {
-    name: "Wales",
-    population: 3063000,
-    capital: "Cardiff"
-  },
-  {
-    name: "England",
-    population: 53010000,
-    capital: "London"
-  },
-  {
-   name: "Northern Ireland",
-   population:  1811000,
-   capital: "Belfast"
- }
-]
+# united_kingdom = [
+#   {
+#     name: "Scotland",
+#     population: 5295000,
+#     capital: "Edinburgh"
+#   },
+#   {
+#     name: "Wales",
+#     population: 3063000,
+#     capital: "Cardiff"
+#   },
+#   {
+#     name: "England",
+#     population: 53010000,
+#     capital: "London"
+#   },
+#   {
+#    name: "Northern Ireland",
+#    population:  1811000,
+#    capital: "Belfast"
+#  }
+# ]
 
-# def find_country_by_name(country,united_kingdom)
-#   for country in united_kingdom
-#     if (country)[:name]
-#       p country
-#     end
-#   end
-# end
-
-total = 0
-for population in united_kingdom
-  total += population[:population]
+def find_country_by_name(country,united_kingdom)
+  for country in united_kingdom
+    if (country)[:name]
+      p country
+    end
+  end
 end
-p total 
+
+# total = 0
+# for population in united_kingdom
+#   total += population[:population]
+# end
+# p total
